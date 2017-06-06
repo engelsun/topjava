aa07b79852cbdc2652fee1efb2b18147d8eef8f9
engelsun <artyom.romenskiy@gmail.com>
2017-06-06 14:43:34 +0300

    HW0 optional with Stream API

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * GKislin
@@ -16,38 +18,29 @@ import java.util.*;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
     }
 
-    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime,
-                                                                    LocalTime endTime, int caloriesLimitPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        Map<LocalDate, Integer> caloriesPerDay = new HashMap<>();
-        for (UserMeal meal :
-                mealList) {
-            LocalDate date = meal.getDateTime().toLocalDate();
-            caloriesPerDay.put(date, meal.getCalories());
-        }
+    public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime,
+                                                                   LocalTime endTime, int caloriesLimitPerDay) {
+        Map<LocalDate, Integer> caloriesPerDay = mealList.stream()
+                .collect(Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate(),
+                        Collectors.summingInt(meal -> meal.getCalories())));
 
-        List<UserMealWithExceed> mealWithExceeds = new ArrayList<>();
-        for (UserMeal meal :
-                mealList) {
-            LocalDateTime dateTime = meal.getDateTime();
-            if (TimeUtil.isBetween(dateTime.toLocalTime(), startTime, endTime)) {
-                int calories = caloriesPerDay.get(dateTime.toLocalDate());
-                mealWithExceeds.add(new UserMealWithExceed(dateTime, meal.getDescription(), meal.getCalories(),
-                        calories > caloriesLimitPerDay));
-            }
-        }
-        return mealWithExceeds;
+        return mealList.stream()
+                .filter(meal -> TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(meal -> {
+                    int calories = caloriesPerDay.get(meal.getDateTime().toLocalDate());
+                    return new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
+                            calories > caloriesLimitPerDay);
+                })
+                .collect(Collectors.toList());
     }
 }
